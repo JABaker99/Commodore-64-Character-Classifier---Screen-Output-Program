65c619fd465eaee83635db9aab71649640d548bd
Jacob Baker <jb00645@my.westga.edu>
2025-10-28 15:25:46 -0400

    Made screen trun to the one color and removed extra words. Also added name and date to top.

---
+++ bak/Main.asm
@@ -0,0 +1,42 @@
+; 10 SYS (2304)
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
+
+
+PRINTLINE=$AB1E
+*=$0900
+
+PROGRAM_START
+
+PROGRAM_END
+        rts
+
+; variable used to print the current letter
+CURRENT_LETTER
+        byte 00 
+        byte 00 
+
+; use this to save a copy of x (the loop counter)
+X_TEMP
+        byte 00
+
+; our input string to process
+INPUT_STRING 
+        text ""@553mb1y r0cks!"
+
+; message to print when a letter is encountered
+IS_LETTER_MESSAGE
+        text " <-- is a letter"
+        byte 13, 00
+
+; message to print when a number is encountered
+IS_NUMBER_MESSAGE
+        text " <-- is a number"
+        byte 13, 00
+
+; message to print when something other than a letter/number is encountered
+IS_SOMETHING_ELSE_MESSAGE
+        text " <-- is some other character"
+        byte 13, 00
